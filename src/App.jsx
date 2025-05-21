6d40e9fda32cca0dd4c421f191ee6de89d14b19c
phoenix1724 <139147142+phoenix1724@users.noreply.github.com>
2025-05-21 12:48:07 +0530

    Day 1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Navbar from './Navbar';
+function App(){
+  return(
+    <div>
+      <Navbar />
+      <h1>Welcome to job board !</h1>
+      <p>Find and apply to your dream tech job today.</p>
+    </div>
+  )
+}
+
+
+
+export default App;
